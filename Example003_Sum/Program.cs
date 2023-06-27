da776bca7c077ff593cb92965d80bd9d241b7381
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-06-27 12:50:10 +0300

    Add Example003_Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int numberA = 12;
+int numberB = 5;
+
+int result = numberA + numberB;
+
+Console.Write("A = ");
+Console.WriteLine(numberA);
+Console.Write("B = ");
+Console.WriteLine(numberB);
+Console.Write("A + B = ");
+Console.WriteLine(result);
